562eafeaa76ab1a4162aa039e67d74aed4a62e22
weaponhsu <huangxu4328@163.com>
2018-12-24 15:12:57 +0800

    20181224 master分支 新增curl类

---
+++ src/Common/RequestHelper.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ *
+ *            ┏┓　　  ┏┓+ +
+ *           ┏┛┻━━━━━┛┻┓ + +
+ *           ┃         ┃ 　
+ *           ┃   ━     ┃ ++ + + +
+ *          ████━████  ┃+
+ *           ┃　　　　　 ┃ +
+ *           ┃　　　┻　　┃
+ *           ┃　　　　　 ┃ + +
+ *           ┗━┓　　　┏━┛
+ *             ┃　　　┃　　　　
+ *             ┃　　　┃ + + + +
+ *             ┃　　　┃    Code is far away from bug with the alpaca protecting
+ *             ┃　　　┃ + 　　　　        神兽保佑,代码无bug
+ *             ┃　　　┃
+ *             ┃　　　┃　　+
+ *             ┃     ┗━━━┓ + +
+ *             ┃         ┣┓
+ *             ┃ 　　　　　┏┛
+ *             ┗┓┓┏━━┳┓┏━┛ + + + +
+ *              ┃┫┫  ┃┫┫
+ *              ┗┻┛  ┗┻┛+ + + +
+ * Created by PhpStorm.
+ * User: weaponhsu
+ * Date: 2018/12/24
+ * Time: 2:52 PM
+ */
+
+namespace youliPhpLib\Common;
+
+
+use Exception;
+use youliPhpLib\ErrMsg\Common\RequestErrMsg;
+
+class RequestHelper
+{
+    static public $sl_cert_path = '';
+    static public $ssl_key_pem = '';
+
+    /**
+     * @param $sl_cert_path
+     * @return $this
+     */
+    static public function setSlCertPath($sl_cert_path)
+    {
+        self::$sl_cert_path = $sl_cert_path;
+    }
+
+    /**
+     * @param $ssl_key_pem
+     * @return $this
+     */
+    static public function setSslKeyPem($ssl_key_pem)
+    {
+        self::$ssl_key_pem = $ssl_key_pem;
+    }
+
+    static public function curlRequest($url, $data = [], $method = 'GET', $headers = [], $use_cert = false, $second = 30)
+    {
+        $ch = curl_init();
+        //设置超时与地址
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        if (strpos($url, 'https://') !== false) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); //严格校验
+        }
+
+        //设置header
+        if(!empty($headers)){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+            curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+        }else{
+            curl_setopt($ch, CURLOPT_HEADER, false);
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+        if ($use_cert === true) {
+            //设置证书
+            if (empty(self::$sl_cert_path))
+                throw new Exception(RequestErrMsg::SL_CERT_PATH_IS_EMPTY, RequestErrMsg::SL_CERT_PATH_IS_EMPTY_NO);
+            else{
+                curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
+                curl_setopt($ch, CURLOPT_SSLCERT, self::$sl_cert_path);
+            }
+
+            if (empty(self::$ssl_key_pem))
+                throw new Exception(RequestErrMsg::SSL_KEY_PEM_IS_EMPTY, RequestErrMsg::SSL_KEY_PEM_IS_EMPTY_NO);
+            else {
+                curl_setopt($ch, CURLOPT_SSLKEYTYPE, 'PEM');
+                curl_setopt($ch, CURLOPT_SSLKEY, self::$ssl_key_pem);
+            }
+        }
+
+        if (strtoupper($method) === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        $document = curl_exec($ch);
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        if(curl_errno($ch)){
+            return new Exception(RequestErrMsg::API_FAILURE . curl_error($ch), '999');
+        }
+        curl_close($ch);
+//        return $document;
+        return [$header_size, $document];
+    }
+
+}
---
+++ src/ErrMsg/Common/RequestErrMsg.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ *            ┏┓　　  ┏┓+ +
+ *           ┏┛┻━━━━━┛┻┓ + +
+ *           ┃         ┃ 　
+ *           ┃   ━     ┃ ++ + + +
+ *          ████━████  ┃+
+ *           ┃　　　　　 ┃ +
+ *           ┃　　　┻　　┃
+ *           ┃　　　　　 ┃ + +
+ *           ┗━┓　　　┏━┛
+ *             ┃　　　┃　　　　
+ *             ┃　　　┃ + + + +
+ *             ┃　　　┃    Code is far away from bug with the alpaca protecting
+ *             ┃　　　┃ + 　　　　        神兽保佑,代码无bug
+ *             ┃　　　┃
+ *             ┃　　　┃　　+
+ *             ┃     ┗━━━┓ + +
+ *             ┃         ┣┓
+ *             ┃ 　　　　　┏┛
+ *             ┗┓┓┏━━┳┓┏━┛ + + + +
+ *              ┃┫┫  ┃┫┫
+ *              ┗┻┛  ┗┻┛+ + + +
+ * Created by PhpStorm.
+ * User: weaponhsu
+ * Date: 2018/12/24
+ * Time: 2:56 PM
+ */
+
+namespace youliPhpLib\ErrMsg\Common;
+
+
+class RequestErrMsg
+{
+    const SL_CERT_PATH_IS_EMPTY = 'cert证书路径不能为空';
+    const SL_CERT_PATH_IS_EMPTY_NO = '300001';
+
+    const SSL_KEY_PEM_IS_EMPTY = 'pem证书路径不能为空';
+    const SSL_KEY_PEM_IS_EMPTY_NO = '300002';
+
+    const API_FAILURE = '接口调用失败';
+    const API_FAILURE_NO = '300003';
+
+}
