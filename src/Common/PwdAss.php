14d475a87aee4d5dd6dc7bc6cd765dede6f86ee5
weaponhsu <huangxu4328@163.com>
2018-12-24 13:22:28 +0800

    20181224 master分支 新增密码加密类

---
+++ src/Common/PwdAss.php
@@ -0,0 +1,334 @@
+<?php
+/**
+ *
+ *            ┏┓　　  ┏┓+ +
+ *           ┏┛┻━━━━━┛┻┓ + +
+ *           ┃         ┃ 　
+ *           ┃   ━     ┃ ++ + + +
+ *          ████━████  ┃+
+ *           ┃　　　　　 ┃ +
+ *           ┃　　　┻　　┃
+ *           ┃　　　　　 ┃ + +
+ *           ┗━┓　　　┏━┛
+ *             ┃　　　┃　　　　
+ *             ┃　　　┃ + + + +
+ *             ┃　　　┃    Code is far away from bug with the alpaca protecting
+ *             ┃　　　┃ + 　　　　        神兽保佑,代码无bug
+ *             ┃　　　┃
+ *             ┃　　　┃　　+
+ *             ┃     ┗━━━┓ + +
+ *             ┃         ┣┓
+ *             ┃ 　　　　　┏┛
+ *             ┗┓┓┏━━┳┓┏━┛ + + + +
+ *              ┃┫┫  ┃┫┫
+ *              ┗┻┛  ┗┻┛+ + + +
+ * Created by PhpStorm.
+ * User: weaponhsu
+ * Date: 2018/12/24
+ * Time: 1:21 PM
+ */
+
+namespace youliPhpLib\Common;
+
+
+class PwdAss
+{
+    public $itoa64;
+    public $iteration_count_log2;
+    public $portable_hashes;
+    public $random_state;
+
+    function __construct($iteration_count_log2 = 0, $protable_hashes = true){
+        $iteration_count_log2 = !empty($iteration_count_log2) && is_numeric($iteration_count_log2) ? $iteration_count_log2 : 0/*$params['iteration_count_log2']*/;
+        $portable_hashes = !empty($protable_hashes) ? $protable_hashes : '';/*$params['portable_hashes']*/;
+
+        $this->itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+        if ($iteration_count_log2 < 4 || $iteration_count_log2 > 31)
+            $iteration_count_log2 = 8;
+        $this->iteration_count_log2 = $iteration_count_log2;
+
+        $this->portable_hashes = $portable_hashes;
+
+//		$this->random_state = microtime() . uniqid(rand(), TRUE); // removed getmypid() for compatibility reasons
+        $this->random_state = microtime();
+        if (function_exists('getmypid'))
+            $this->random_state .= getmypid();
+    }
+
+    function get_random_bytes($count)
+    {
+        $output = '';
+        if ( @is_readable('/dev/urandom') &&
+            ($fh = @fopen('/dev/urandom', 'rb'))) {
+            $output = fread($fh, $count);
+            fclose($fh);
+        }
+
+        if (strlen($output) < $count) {
+            $output = '';
+            for ($i = 0; $i < $count; $i += 16) {
+                $this->random_state =
+                    md5(microtime() . $this->random_state);
+                $output .=
+                    pack('H*', md5($this->random_state));
+            }
+            $output = substr($output, 0, $count);
+        }
+
+        return $output;
+    }
+
+    function encode64($input, $count)
+    {
+        $output = '';
+        $i = 0;
+        do {
+            $value = ord($input[$i++]);
+            $output .= $this->itoa64[$value & 0x3f];
+            if ($i < $count)
+                $value |= ord($input[$i]) << 8;
+            $output .= $this->itoa64[($value >> 6) & 0x3f];
+            if ($i++ >= $count)
+                break;
+            if ($i < $count)
+                $value |= ord($input[$i]) << 16;
+            $output .= $this->itoa64[($value >> 12) & 0x3f];
+            if ($i++ >= $count)
+                break;
+            $output .= $this->itoa64[($value >> 18) & 0x3f];
+        } while ($i < $count);
+
+        return $output;
+    }
+
+    function gensalt_private($input)
+    {
+        $output = '$P$';
+        $output .= $this->itoa64[min($this->iteration_count_log2 +
+            ((PHP_VERSION >= '5') ? 5 : 3), 30)];
+        $output .= $this->encode64($input, 6);
+
+        return $output;
+    }
+
+    function crypt_private($password, $setting)
+    {
+        $output = '*0';
+        if (substr($setting, 0, 2) == $output){
+            $output = '*1';
+        }
+
+        $id = substr($setting, 0, 3);
+        # We use "$P$", phpBB3 uses "$H$" for the same thing
+        if ($id != '$P$' && $id != '$H$'){
+            return $output;
+        }
+
+        $count_log2 = strpos($this->itoa64, $setting[3]);
+        if ($count_log2 < 7 || $count_log2 > 30)
+            return $output;
+
+        $count = 1 << $count_log2;
+
+        $salt = substr($setting, 4, 8);
+        if (strlen($salt) != 8)
+            return $output;
+
+        # We're kind of forced to use MD5 here since it's the only
+        # cryptographic primitive available in all versions of PHP
+        # currently in use.  To implement our own low-level crypto
+        # in PHP would result in much worse performance and
+        # consequently in lower iteration counts and hashes that are
+        # quicker to crack (by non-PHP code).
+        if (PHP_VERSION >= '5') {
+            $hash = md5($salt . $password, TRUE);
+            do {
+                $hash = md5($hash . $password, TRUE);
+            } while (--$count);
+        } else {
+            $hash = pack('H*', md5($salt . $password));
+            do {
+                $hash = pack('H*', md5($hash . $password));
+            } while (--$count);
+        }
+
+        $output = substr($setting, 0, 12);
+        $output .= $this->encode64($hash, 16);
+
+        return $output;
+    }
+
+    function gensalt_extended($input)
+    {
+        $count_log2 = min($this->iteration_count_log2 + 8, 24);
+        # This should be odd to not reveal weak DES keys, and the
+        # maximum valid value is (2**24 - 1) which is odd anyway.
+        $count = (1 << $count_log2) - 1;
+
+        $output = '_';
+        $output .= $this->itoa64[$count & 0x3f];
+        $output .= $this->itoa64[($count >> 6) & 0x3f];
+        $output .= $this->itoa64[($count >> 12) & 0x3f];
+        $output .= $this->itoa64[($count >> 18) & 0x3f];
+
+        $output .= $this->encode64($input, 3);
+
+        return $output;
+    }
+
+    function gensalt_blowfish($input)
+    {
+        # This one needs to use a different order of characters and a
+        # different encoding scheme from the one in encode64() above.
+        # We care because the last character in our encoded string will
+        # only represent 2 bits.  While two known implementations of
+        # bcrypt will happily accept and correct a salt string which
+        # has the 4 unused bits set to non-zero, we do not want to take
+        # chances and we also do not want to waste an additional byte
+        # of entropy.
+        $itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        $output = '$2a$';
+        $output .= chr(ord('0') + $this->iteration_count_log2 / 10);
+        $output .= chr(ord('0') + $this->iteration_count_log2 % 10);
+        $output .= '$';
+
+        $i = 0;
+        do {
+            $c1 = ord($input[$i++]);
+            $output .= $itoa64[$c1 >> 2];
+            $c1 = ($c1 & 0x03) << 4;
+            if ($i >= 16) {
+                $output .= $itoa64[$c1];
+                break;
+            }
+
+            $c2 = ord($input[$i++]);
+            $c1 |= $c2 >> 4;
+            $output .= $itoa64[$c1];
+            $c1 = ($c2 & 0x0f) << 2;
+
+            $c2 = ord($input[$i++]);
+            $c1 |= $c2 >> 6;
+            $output .= $itoa64[$c1];
+            $output .= $itoa64[$c2 & 0x3f];
+        } while (1);
+
+        return $output;
+    }
+
+    function HashPassword($password)
+    {
+        $random = '';
+
+        if (CRYPT_BLOWFISH == 1 && !$this->portable_hashes) {
+            $random = $this->get_random_bytes(16);
+            $hash =
+                crypt($password, $this->gensalt_blowfish($random));
+            if (strlen($hash) == 60)
+                return $hash;
+        }
+
+        if (CRYPT_EXT_DES == 1 && !$this->portable_hashes) {
+            if (strlen($random) < 3)
+                $random = $this->get_random_bytes(3);
+            $hash =
+                crypt($password, $this->gensalt_extended($random));
+            if (strlen($hash) == 20)
+                return $hash;
+        }
+
+        if (strlen($random) < 6)
+            $random = $this->get_random_bytes(6);
+        $hash =
+            $this->crypt_private($password,
+                $this->gensalt_private($random));
+        if (strlen($hash) == 34)
+            return $hash;
+
+        # Returning '*' on error is safe here, but would _not_ be safe
+        # in a crypt(3)-like function used _both_ for generating new
+        # hashes and for validating passwords against existing hashes.
+        return '*';
+    }
+
+    function CheckPassword($password, $stored_hash){
+        $hash = $this->crypt_private($password, $stored_hash);
+        if ($hash[0] == '*'){
+            $hash = crypt($password, $stored_hash);
+        }
+        return $hash === $stored_hash;
+    }
+
+    /**
+     * @param string $string 原文或者密文
+     * @param string $operation 操作(ENCODE | DECODE), 默认为 DECODE
+     * @param string $key 密钥
+     * @param int $expiry 密文有效期, 加密时候有效， 单位 秒，0 为永久有效
+     * @return string 处理后的 原文或者 经过 base64_encode 处理后的密文
+     *
+     * @example
+     *
+     * $a = authcode('abc', 'ENCODE', 'key');
+     * $b = authcode($a, 'DECODE', 'key');  // $b(abc)
+     *
+     * $a = authcode('abc', 'ENCODE', 'key', 3600);
+     * $b = authcode('abc', 'DECODE', 'key'); // 在一个小时内，$b(abc)，否则 $b 为空
+     */
+    public static function authcode($string, $operation = 'DECODE', $key = '', $expiry = 3600) {
+
+        $ckey_length = 4;
+        // 随机密钥长度 取值 0-32;
+        // 加入随机密钥，可以令密文无任何规律，即便是原文和密钥完全相同，加密结果也会每次不同，增大po,jie难度。
+        // 取值越大，密文变动规律越大，密文变化 = 16 的 $ckey_length 次方
+        // 当此值为 0 时，则不产生随机密钥
+
+
+        $key = md5 ( $key ? $key : 'key' ); //这里可以填写默认key值
+        $keya = md5 ( substr ( $key, 0, 16 ) );
+        $keyb = md5 ( substr ( $key, 16, 16 ) );
+        $keyc = $ckey_length ? ($operation == 'DECODE' ? substr ( $string, 0, $ckey_length ) : substr ( md5 ( microtime () ), - $ckey_length )) : '';
+
+        $cryptkey = $keya . md5 ( $keya . $keyc );
+        $key_length = strlen ( $cryptkey );
+
+        $string = $operation == 'DECODE' ? base64_decode ( substr ( $string, $ckey_length ) ) : sprintf ( '%010d', $expiry ? $expiry + time () : 0 ) . substr ( md5 ( $string . $keyb ), 0, 16 ) . $string;
+        $string_length = strlen ( $string );
+
+        $result = '';
+        $box = range ( 0, 255 );
+
+        $rndkey = array ();
+        for($i = 0; $i <= 255; $i ++) {
+            $rndkey [$i] = ord ( $cryptkey [$i % $key_length] );
+        }
+
+        for($j = $i = 0; $i < 256; $i ++) {
+            $j = ($j + $box [$i] + $rndkey [$i]) % 256;
+            $tmp = $box [$i];
+            $box [$i] = $box [$j];
+            $box [$j] = $tmp;
+        }
+
+        for($a = $j = $i = 0; $i < $string_length; $i ++) {
+            $a = ($a + 1) % 256;
+            $j = ($j + $box [$a]) % 256;
+            $tmp = $box [$a];
+            $box [$a] = $box [$j];
+            $box [$j] = $tmp;
+            $result .= chr ( ord ( $string [$i] ) ^ ($box [($box [$a] + $box [$j]) % 256]) );
+        }
+
+        if ($operation == 'DECODE') {
+            if ((substr ( $result, 0, 10 ) == 0 || substr ( $result, 0, 10 ) - time () > 0) && substr ( $result, 10, 16 ) == substr ( md5 ( substr ( $result, 26 ) . $keyb ), 0, 16 )) {
+                return substr ( $result, 26 );
+            } else {
+                return '';
+            }
+        } else {
+            return $keyc . str_replace ( '=', '', base64_encode ( $result ) );
+        }
+
+    }
+}
